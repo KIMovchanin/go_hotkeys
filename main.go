498bdf3cdd8e2bf6edb5c25e7d5f5715134705e1
KIMovchanin <kondr2004k@gmail.com>
2026-05-10 18:58:11 +0400

    added 'add' argument which give you apportunety to add new hotkeys

--- main.go
+++ main.go
@@ -15,10 +15,50 @@ import (
 )
 
 func main() {
+	// Если длина предаваемых через консоль аргументов больше 1
+	// и этот аргумет "add", то запустить addBind().
+	if len(os.Args) > 1 && os.Args[1] == "add" {
+		addBind()
+		return
+	}
+
 	// mainthread.Init нужен, чтобы библиотека hotkey нормально работала
 	// с системным event loop. В документации он указан как способ запускать
 	// self-contained приложения с hotkey.
 	mainthread.Init(run)
+}
+
+func addBind() {
+	// Ожидаем команду такого вида:
+	// go run . add "Name" "Ctrl+Alt+R" "target"
+	if len(os.Args) != 5 {
+		fmt.Println(`Usage: go run . add "Name" "Ctrl+Alt+R" "Target"`)
+		return
+	}
+
+	// Создаём из полученных аргументов горячую клавишу
+	newBind := config.Bind{
+		Name:   os.Args[2],
+		Hotkey: os.Args[3],
+		Target: os.Args[4],
+	}
+
+	if _, _, err := hotkeys.ParseHotKey(newBind.Hotkey); err != nil {
+		log.Fatal("Invalid hotkey:", err)
+	}
+
+	binds, err := config.Load("config.json")
+	if err != nil {
+		log.Fatal("Failed to load config:", err)
+	}
+
+	binds = append(binds, newBind)
+
+	if err := config.Save("config.json", binds); err != nil {
+		log.Fatal("Failed to save config:", err)
+	}
+
+	fmt.Println("Added: ", newBind.Hotkey, "->", newBind.Name)
 }
 
 func run() {
